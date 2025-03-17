dd690631cf3ef978130365d45176d3ad61b1ceed
vincent <vincent19901026@gmail.com>
2025-03-17 15:26:39 +0800

    fix custom views

--- src/agent/custom_views.py
+++ src/agent/custom_views.py
@@ -20,12 +20,12 @@ class CustomAgentStepInfo:
 class CustomAgentBrain(BaseModel):
     """Current state of the agent"""
 
-    prev_action_evaluation: str
+    evaluation_previous_goal: str
     important_contents: str
     task_progress: str
     future_plans: str
     thought: str
-    summary: str
+    next_goal: str
 
 
 class CustomAgentOutput(AgentOutput):
@@ -34,22 +34,21 @@ class CustomAgentOutput(AgentOutput):
     @dev note: this model is extended with custom actions in AgentService. You can also use some fields that are not in this model as provided by the linter, as long as they are registered in the DynamicActions model.
     """
 
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-
     current_state: CustomAgentBrain
-    action: list[ActionModel]
 
     @staticmethod
     def type_with_custom_actions(
         custom_actions: Type[ActionModel],
     ) -> Type["CustomAgentOutput"]:
         """Extend actions with custom actions"""
-        return create_model(
+        model_ = create_model(
             "CustomAgentOutput",
             __base__=CustomAgentOutput,
             action=(
                 list[custom_actions],
-                Field(...),
+                Field(..., description='List of actions to execute', json_schema_extra={'min_items': 1}),
             ),  # Properly annotated field with no default
             __module__=CustomAgentOutput.__module__,
         )
+        model_.__doc__ = 'AgentOutput model with custom actions'
+        return model_
